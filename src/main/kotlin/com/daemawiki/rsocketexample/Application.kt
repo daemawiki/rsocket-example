4412092c5c436565cc36650c62d4d8e103a08bf0
101 <tmddnjs001133@dsm.hs.kr>
2024-11-06 23:06:39 +0900

    setting: 기본 셋팅

--- src/main/kotlin/com/daemawiki/rsocketexample/Application.kt
+++ src/main/kotlin/com/daemawiki/rsocketexample/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class RsocketExampleApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<RsocketExampleApplication>(*args)
+    runApplication<Application>(*args)
 }
