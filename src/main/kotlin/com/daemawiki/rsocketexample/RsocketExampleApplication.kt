489483b1b760ce64399a778eb8dc1f4c44b27ba6
101 <tmddnjs001133@dsm.hs.kr>
2024-11-06 23:04:17 +0900

    init: project

---
+++ src/main/kotlin/com/daemawiki/rsocketexample/RsocketExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.daemawiki.rsocketexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RsocketExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<RsocketExampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/daemawiki/rsocketexample/RsocketExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.daemawiki.rsocketexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RsocketExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
